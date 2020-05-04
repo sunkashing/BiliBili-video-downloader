5d28e2049d303b659df09d513c8c490afb94396f
sunkashing <sunjiacheng1111@gmail.com>
2020-05-04 00:06:05 -0400

    mac version completed

---
+++ src/processor/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from src.processor.join_flv import concat_flv
+from src.processor.join_mp4 import concat_mp4
+from src.processor.ffmpeg import *
+from src.processor.rtmpdump import *
--- video_downloader.py
+++ video_downloader.py
@@ -1,9 +1,6 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QDesktopWidget, QFrame
-
-import bilibili
-from video_downloader_GUI import MyWidget
+from src.video_downloader_GUI import MyWidget
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
